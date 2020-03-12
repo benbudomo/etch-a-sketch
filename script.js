b96043afe46f75f4eb9dfae91c88df87191dec35
Ben Budomo <luminous.talisman@protonmail.com>
2020-03-12 00:30:22 -0500

    Create hover function that changes background color

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function createGrid(rows, columns) {
 
     for(let i = 0; i < (rows * columns); i++) {
         let newDiv = document.createElement('div');
+        newDiv.addEventListener('mouseover', e => e.target.classList.add('backgroundColor'));
         squareGrid.appendChild(newDiv).className = 'actualSquare'
     }
 }
 
 createGrid(16, 16);
+
