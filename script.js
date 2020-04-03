eff0853a38a8ab1a422089bef870b1c6e01d5eae
Ben Budomo <luminous.talisman@protonmail.com>
2020-04-02 19:56:59 -0500

    Create function that randomly changes box color when hovered

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const squareGrid = document.getElementById('squareGrid');
+let userNumber = 8;
 
 function createGrid(rows, columns) {
     squareGrid.style.setProperty('--gridRows', rows);
@@ -18,11 +19,22 @@ function clearGrid() {
     });
 }
 
-function myFunction() {
-    let userNumber = prompt('Size of grid?', '64');
+function newGrid() {
+    let userNumber = prompt('Size of grid:', '16');
     clearGrid();
     createGrid(userNumber, userNumber);
 }
 
-createGrid(16, 16);
+function randomColors() {
+    newGrid();
+    let currentDivs = document.querySelectorAll('.actualSquare');
+    currentDivs.forEach(div => {
+        div.addEventListener('mouseover', (e) => {
+            let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            div.style.backgroundColor = randomColor;
+        })
+    });
+}
+
+createGrid(userNumber, userNumber);
 
